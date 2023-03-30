ece026a05b142ac57f752bbc8834106ed084f304
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-03-30 00:46:53 -0300

    Create builder.py

---
+++ builder.py
@@ -0,0 +1,5 @@
+import os
+
+print("I don't tested TODAY this builder, if not work notify me (4am xd)")
+print("BTW. Do you need install ALL requeriments, include pyarmor + pyinstaller. PYTHON 3.8.9 !!!!")
+os.system("pyarmor pack -e --onefile --noconsole --icon NONE setup.py")

